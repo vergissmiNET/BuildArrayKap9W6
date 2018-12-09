3877d70fb1af6cd6ea412c43116c3069166079e4
vergissmiNET <kontakt@vergissmi.net>
2018-12-09 10:46:34 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// testwerte
+        double x0 = 0.0;
+        double x1 = 10.0;
+        int increments = 10;
+        System.out.println("Array Index " + increments + " hat den Wert " + buildArray(x0,x1,increments));
+    }
+    private static double buildArray(double x0, double x1, int size) {
+        double[] arr = Array
+    return 0.0;
+    }
+}
